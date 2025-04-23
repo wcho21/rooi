8ce475b5c36e1961ee32e454d49e7018bbfa9d7b
Cho <wcho.dev@gmail.com>
2025-04-23 15:41:10 +0000

    install tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-import "$lib/slidypage/global-style.css";
-const { children } = $props();
+  import "../app.css";
+  const { children } = $props();
 </script>
 
 {@render children()}
