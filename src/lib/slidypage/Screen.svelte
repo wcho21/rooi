06c0d4fd575984f0812040f63cc3c2225fd28903
Cho <wcho.dev@gmail.com>
2025-04-24 18:50:19 +0000

    hide scrollbar

--- src/lib/slidypage/Screen.svelte
+++ src/lib/slidypage/Screen.svelte
@@ -3,3 +3,9 @@
 </script>
 
 <div class="border-box w-dvw h-dvh snap-mandatory snap-y overflow-y-scroll scroll-smooth">{@render children()}</div>
+
+<style lang="postcss">
+  div {
+    scrollbar-width: none;
+  }
+</style>
